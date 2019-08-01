9bc2a48d8fbc0c6700c380d37ab3279c0401c779
unknown <610140@CS.MUM.EDU>
2019-08-01 11:57:38 -0500

    Add sample ui

---
+++ ui/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res){
+   res.render('hello_world');
+});
+
+app.listen(3000);
