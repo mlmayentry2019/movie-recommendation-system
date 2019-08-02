1ecabc0e5fc9921657cab0c21ca88bcc0281c531
hoanglt705 <hoanglt705@gmail.com>
2019-08-02 15:04:03 -0500

    Improve loading csv file in api

--- api/docker/app.py
+++ api/docker/app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route('/top_trend', methods=['GET'])
 def topTrend():
-    df_rating = pd.read_csv('./data/ratings_small.csv')
     top_rating = df_rating.nlargest(20, ['rating', 'timestamp'])
     top_rating_list = top_rating['movieId'].astype(str).tolist()
-
-    df_movie = pd.read_csv('./data/movies_metadata.csv')
+    
     df_movie['id'] = df_movie['id'].astype(str)
     top_trend_movies = df_movie[df_movie['id'].isin(top_rating_list)]
     return top_trend_movies.to_json()
 
 if __name__ == '__main__':
+    df_rating = pd.read_csv('./data/ratings_small.csv')
+    df_movie = pd.read_csv('./data/movies_metadata.csv')
     app.run(debug=True, host='0.0.0.0')
