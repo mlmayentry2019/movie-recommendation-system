b92e3e599670fda048676b30bb2ca5c150986402
hoanglt705 <hoanglt705@gmail.com>
2019-08-02 13:42:05 -0500

    Implement ui of toptrend

--- ui/app.js
+++ ui/app.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
+var rp = require('request-promise');
+
 
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.get('/', function(req, res){
-   res.render('hello_world');
+app.get('/', async function(req, res) {
+   try {
+      var response = await rp("http://localhost:5000/top_trend");
+      obj = JSON.parse(response);
+      res.render('home', {name: obj.title});
+   } catch(err) {
+      console.error(err)
+   }
 });
 
 app.listen(3000);
